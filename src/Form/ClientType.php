ba7a9573b912068f3b4395997d4636a9b3409e85
landrykoffi <lkoffi@ebenyx.com>
2023-05-18 15:22:52 +0000

    maj

---
+++ src/Form/ClientType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('code')
+            ->add('libelle')
+            ->add('responsable')
+            ->add('adresse')
+            ->add('ville')
+            ->add('tel')
+            ->add('email')
+            ->add('matfiscal')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
---
+++ src/Form/FournisseurType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Fournisseur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FournisseurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('code')
+            ->add('libelle')
+            ->add('entreprise')
+            ->add('responsable')
+            ->add('adresse')
+            ->add('ville')
+            ->add('tel')
+            ->add('email')
+            ->add('matfiscal')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Fournisseur::class,
+        ]);
+    }
+}
